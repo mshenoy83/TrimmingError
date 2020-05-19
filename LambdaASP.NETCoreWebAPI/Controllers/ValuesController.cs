dbae72ed625bd82335ab951870e88b205eb49a9c
Madhav <mshenoy@orbital8.com.au>
2020-05-19 11:26:54 +1000

    push code

---
+++ LambdaASP.NETCoreWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaASP.NETCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public Task<string[]> Get()
+        {
+            return Task.FromResult(new string[] {"value1", "value2"});
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
